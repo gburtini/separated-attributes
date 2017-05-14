603734dcc688a1626a8e413b268376e1af5643ad
Giuseppe Burtini <joe@truephp.com>
2017-05-14 16:58:28 -0700

    Linting.

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,5 +1,5 @@
 const expect = require('chai').expect;
-const {   
+const {
   isSeparatedAttribute,
   splitAttribute,
   splitAttributes,
@@ -7,11 +7,11 @@ const {
 
 describe('isSeparatedAttribute', () => {
   it('should return true for class, rel, rev, style, media', () => {
-    ['class', 'rel', 'rev', 'style', 'media'].map((attribute) => {
+    ['class', 'rel', 'rev', 'style', 'media'].map(attribute => {
       expect(isSeparatedAttribute(attribute)).to.be.ok;
-    })
+    });
   });
-  
+
   it('should indicate we split on a space in classes', () => {
     expect(isSeparatedAttribute('class')).to.be.eq(' ');
   });
@@ -21,14 +21,12 @@ describe('isSeparatedAttribute', () => {
   });
 });
 
-
 describe('splitAttribute', () => {
   it('should successfully split a style', () => {
     const splitStyle = splitAttribute('style', 'font-family:a,b,c; color:red');
-    expect(splitStyle).to.be.deep.equal([ 'font-family:a,b,c', 'color:red' ]);
+    expect(splitStyle).to.be.deep.equal(['font-family:a,b,c', 'color:red']);
   });
 });
-
 
 describe('splitAttributes', () => {
   it('should successfully split a map', () => {
@@ -41,7 +39,7 @@ describe('splitAttributes', () => {
     const split = splitAttributes(map);
     expect(split).to.be.deep.equal({
       class: ['a', 'b', 'c'],
-      style: [ 'font-family:a,b,c', 'color:red' ],
+      style: ['font-family:a,b,c', 'color:red'],
       name: 'alphaghettis with spaces, and commas; and semicolons',
     });
   });
